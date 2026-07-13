45121292f08edfa903f70ea13532e9c9d83f3414
adel_mhmd77 <am3507962@gmail.com>
2026-07-13 10:02:25 +0300

    Add responsive navbar and favorite toggle functionality; update HTML structure and content

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ *  navbar toggle in mobile
+ */
+
+const /**{NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /**{NodeElement} */ $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click",()=> $navbar.classList.toggle("active"));
+
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+
+});
+
+
+// add to favorite button toggle
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+$toggleBtns.forEach(
+    $toggleBtn =>{
+        $toggleBtn.addEventListener("click",()=>{
+            $toggleBtn.classList.toggle("active");
+        })
+    }
+)
